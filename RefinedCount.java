7affadcf4e6718fb99fb33e8b86b043da4de904e
bkunz4 <136372473+bkunz4@users.noreply.github.com>
2023-11-16 15:58:19 +0000

    final

--- RefinedCount.java
+++ RefinedCount.java
@@ -2,65 +2,68 @@ import java.util.*;
 import java.io.*;
 
 public class RefinedCount {
-  public static void main(String[] args) throws Exception {
-    // Check command line length
-    if (args.length != 1) {
-      System.out.println("Usage: java sourceFile");
-      System.exit(1);
+    public static void main(String[] args) throws Exception {    
+        if (args.length != 1) {
+            System.out.println("Usage: java CountKeywords <Java source file>");
+            System.exit(1);
+        }
+
+        String filename = args[0];
+        File file = new File(filename);
+
+        if (file.exists()) {
+            System.out.println("The number of keywords in " + filename + " is " + countKeywords(file));
+        } else {
+            System.out.println("File " + filename + " does not exist");
+        }
     }
 
-    // Check if file exists and display keyword count
-    File file = new File(args[0]);
-    if (file.exists()) {
-      System.out.println("The number of keywords in " + args[0] 
-        + " is " + countKeywords(file));
+    public static int countKeywords(File file) throws Exception {    // Array of all Java keywords + true, false and null
+        String[] keywordString = {"abstract", "assert", "boolean",
+			"break", "byte", "case", "catch", "char", "class", "const",
+			"continue", "default", "do", "double", "else", "enum",
+			"extends", "for", "final", "finally", "float", "goto",
+			"if", "implements", "import", "instanceof", "int",
+			"interface", "long", "native", "new", "package", "private",
+			"protected", "public", "return", "short", "static",
+			"strictfp", "super", "switch", "synchronized", "this",
+			"throw", "throws", "transient", "try", "void", "volatile",
+			"while", "true", "false", "null"};
+
+        Set<String> keywordSet = new HashSet<>(Arrays.asList(keywordString));
+        int count = 0;
+        boolean inCommentBlock = false;
+
+        try (Scanner input = new Scanner(file)) {
+            while (input.hasNext()) {
+                String word = input.next();
+
+                // Checks for comments
+                if (word.startsWith("/*")) {
+                    inCommentBlock = true;
+                    // Continues to the next iteration without checking the keyword
+                    continue;
+                }
+
+                // Checks for the end of a comment block
+                if (inCommentBlock && word.endsWith("*/")) {
+                    inCommentBlock = false;
+                    // Continues to the next iteration without checking the keyword
+                    continue;
+                }
+
+                // Skips single-line comments
+                if (word.startsWith("//")) {
+                    break; // Assumes line comments do not overlap with paragraph comments
+                }
+
+                // If not in a comment block, it checks for keywords
+                if (!inCommentBlock && keywordSet.contains(word)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
-    else {
-      System.out.println("The file " + args[0] + " does not exist");
-    }
-  }
-
-  /** Method returns the keyword count */
-  public static int countKeywords(File file) throws Exception {
-    // Array of all Java Keywords + ture, false and null
-    String[] keywordString = {"abstract", "assert", "boolean",
-      "break", "byte", "case", "catch", "char", "class", "const",
-      "continue", "default", "do", "double", "else", "enum",
-      "extends", "for", "final", "finally", "float", "goto",
-      "if", "implements", "import", "instanceof", "int",
-      "interface", "long", "native", "new", "package", "private",
-      "protected", "public", "return", "short", "static",
-      "strictfp", "super", "switch", "synchronized", "this",
-      "throw", "throws", "transient", "try", "void", "volatile",
-      "while", "true", "false", "null"};
-
-    Set<String> keywordSet = 
-      new HashSet<>(Arrays.asList(keywordString));
-    int count = 0;
-
-    Scanner input = new Scanner(file);
-
-    while (input.hasNext()) {
-      String word = input.next();
-      if (word.equals("//")) { // Don't count comments
-        input.nextLine(); 
-      }
-      else if (word.contains("\"")) { // Don't count string
-        String nextWord;
-        do {
-          nextWord = input.next();
-        } while (!nextWord.contains("\""));
-      }
-      else if (word.contains("/*")) { // Don't count block comments
-        String nextWord;
-        do {
-          nextWord = input.next();
-        } while (!nextWord.contains("*/"));
-      }
-      else if (keywordSet.contains(word))
-        count++;
-    }
-
-    return count;
-  }
 }
